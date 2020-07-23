0f698ed6ca2b279b07258cf732815dadb7a9fad7
Adway Wadekar <adway@adway.io>
2020-07-23 11:06:34 -0400

    Target _blank

--- components/nav.js
+++ components/nav.js
@@ -100,7 +100,7 @@ export default () => (
       <Link href='/journalism' passHref>
         <NavLink>Journalism</NavLink>
       </Link>
-      <a href='https://blog.adway.io'>
+      <a href='https://blog.adway.io' target='_blank'>
         <NavLink>Blog</NavLink>
       </a>
       <ColorSwitcher />
