fdcd3b74d2d6bd3c0102f50997e5d1ee958c29f4
Adway Wadekar <adway@adway.io>
2020-12-21 23:52:58 -0500

    E-mail

--- components/social.js
+++ components/social.js
@@ -4,12 +4,13 @@ import {Instagram, Linkedin, Mail, Twitter} from 'react-feather'
 export const Social = ({children, sx, size = 25}) => (
 <>
   <Flex sx={{
-    m: 'auto', justifyContent: 'center', mt: 3, a: {px: 2} ,
+    m: 'auto', justifyContent: 'center', mt: 1, a: {px: 2}, fontSize: 0,
     ...sx}}>
-    <a href="https://instagram.com/_adway"><Instagram size = {size}/></a>
+    {/* <a href="https://instagram.com/_adway"><Instagram size = {size}/></a>
     <a href="https://linkedin.com/adwaywadekar"><Linkedin size = {size}/></a>
     <a href="https://twitter.com/_adway"><Twitter size = {size}/></a>
-    <a href="mailto:adway@adway.io"><Mail size = {size}/></a>
+    <a href="mailto:adway@adway.io"><Mail size = {size}/></a> */}
+    e-mail: <a href="mailto:adway@adway.io">adway@adway.io</a>
   </Flex>
 </>
 )
